f6551952e3156582aa1f005664a31d970c4c8f2e
Pawan <funwithcodings@gmail.com>
2024-12-08 10:51:25 +0000

    initialize deno project and add encode and decode message

---
+++ rpc.ts
@@ -0,0 +1,68 @@
+
+interface Message {
+  jsonrpc:"2.0"
+}
+
+export interface RequestMessage extends Message {
+  method:string,
+  id:number,
+  params?:unknown,
+
+}
+
+export interface ResponseMessage extends Message {
+  id:number|null,
+  result?:unknown,
+  error?:unknown,
+}
+export function encodeMessage(msg:ResponseMessage):string{
+  const msgStr = JSON.stringify(msg);
+  return `Content-Length: ${msgStr.length}\r\n\r\n${msgStr}`
+}
+
+export function decodeMessage(bytes:Uint8Array):RequestMessage{
+
+  try {
+
+    const [headerBytes, contentBytes] = splitBytes(bytes, "\r\n\r\n");
+    const decoder = new TextDecoder();
+    const header = decoder.decode(headerBytes);
+    const contentLength = Number(header.split(":")[1]);
+    if(isNaN(contentLength)){
+      throw new Error("Content Length Not Found!")
+    }
+    const content = decoder.decode(contentBytes.subarray(0, contentLength));
+    try {
+      return JSON.parse(content) as RequestMessage;
+    } catch {
+      throw new Error("Invalid Json")
+    }
+  } catch(err) {
+    throw err;
+  }
+
+}
+
+function splitBytes(bytes:Uint8Array, seprator:string):[Uint8Array, Uint8Array]{
+  const encoder = new TextEncoder();
+  const separatorBytes = encoder.encode(seprator);
+  let index = -1;
+  for(let i=0; i<bytes.length - separatorBytes.length; i++){
+    let match = true;
+    for(let j=0; j<separatorBytes.length; j++){
+      if(bytes[i+j]!==separatorBytes[j]){
+        match = false;
+        break;
+      }
+    }
+    if(match){
+      index = i;
+      break;
+    }
+  }
+  if(index==-1){
+    throw new Error("Seperator not found!")
+  }
+  return [bytes.subarray(0, index), bytes.subarray(index+separatorBytes.length, bytes.length)]
+
+}
---
+++ rpc_test.ts
@@ -0,0 +1,29 @@
+import { assertEquals } from "@std/assert/equals";
+import { decodeMessage, encodeMessage, RequestMessage, ResponseMessage } from "./rpc.ts";
+
+
+
+Deno.test(function testEncodeMessage(){
+
+  const testData:ResponseMessage = {
+    jsonrpc:"2.0",
+    id:4
+  }
+  const expected = 'Content-Length: 24\r\n\r\n{"jsonrpc":"2.0","id":4}'
+  const encodeData = encodeMessage(testData);
+
+  assertEquals(expected, encodeData)
+})
+Deno.test(function testDecodeMessage(){
+  const testData:RequestMessage = {
+    jsonrpc:"2.0",
+    method:"initialize",
+    id:4
+  }
+  const byteData = new TextEncoder().encode(`Content-Length: 46\r\n\r\n${JSON.stringify(testData)}`)
+  const decodedData = decodeMessage(byteData);
+
+  assertEquals(decodedData.method, "initialize")
+  assertEquals(decodedData.id, 4)
+  assertEquals(decodedData.jsonrpc, "2.0")
+})
